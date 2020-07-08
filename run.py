1141bfaba99181b450bb0d7b050fdd6aa45b15c1
duanbibo <820355197@qq.com>
2020-07-08 16:36:49 +0800

    7/8 项目中配置钉钉webhook地址

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 print("hello word")
 
-print("钉钉 webhook Test2")
+print(" webhook begin")
+import requests
+
+#自定义钉钉机器人的url
+url="https://oapi.dingtalk.com/robot/send?access_token=878e9edb2c45ef7aab6024d64c76120a1397991e9d07fca14853ff6e5b61e659"
+
+headers = {
+"Content-Type": "application/json ;charset=utf-8 "
+}
+body={
+      "msgtype": "text",
+      "text": {
+            "content": "测试执行成功，查看测试报告，http://121.36.102.233:8080/"
+      }
+}
+r=requests.request("post",url=url,headers=headers,json=body)
+
+print("webhooks end")
